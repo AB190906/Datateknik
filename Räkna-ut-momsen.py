15f67f135077168c5f192abd1c4e8181655730df
ahmed.awadhhelalale <ahmadalanazi166@gmail.com>
2022-09-27 20:33:58 +0200

    Moms-price

---
+++ Räkna-ut-momsen.py
@@ -0,0 +1,15 @@
+moms=0.25
+price=input("Skriv in pris")
+print("The price is", price)
+price_moms = float(price)*moms
+print("The moms from the price is", price_moms)
+print("The rest of the price is", float(price)-moms)
+
+
+
+
+
+
+
+
+
