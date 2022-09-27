324b49cf131d1d950b69e621586beba0a4871b4f
ahmed.awadhhelalale <ahmadalanazi166@gmail.com>
2022-09-27 19:45:35 +0200

    test

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Ahmed")
+a = 1
+b =6
+c = 5.2
+d = 5.2
+e = 3
+aa = 1 + a
+bb =1 + b
+cc = 5.2 + c
+dd =5.2 + d
+ee =3 + e
+print( aa )
+print( bb )
+print( cc )
+print( dd )
+print( ee )
+aa = 10 + 1
+bb = 10 + b
+cc = 10 + c
+dd = 10 + 5.2
+ee = 10 + 3
+print( aa )
+print( bb )
+print( cc )
+print( dd )
+print( ee )
